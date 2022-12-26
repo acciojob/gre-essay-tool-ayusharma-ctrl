089bd4bc8bc69cde67e2e50c8c96cdc8ecd25f91
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2022-12-26 22:35:42 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+let evaluatedText = document.getElementById('evaluatedText')
+let wordCount = document.getElementById('wordCount')
+
+evaluatedText.onkeyup = function () {
+    var spaceCount = 0;
+    var text = evaluatedText.value.trim()
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] != ' ' && text[i + 1] == ' ') {
+            spaceCount++
+        }
+    }
+
+    if (evaluatedText.value == '') {
+        wordCount.innerText = 0
+    }
+    else{
+        wordCount.innerText = spaceCount + 1
+    }
+}
